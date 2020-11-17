6d652b122409761ad83b9aa96fd3e5a86a73f71b
Robert Barrow <merefield@gmail.com>
2020-11-17 16:06:41 +0000

    added post alerter tests

--- spec/lib/updater_spec.rb
+++ spec/lib/updater_spec.rb
@@ -4,6 +4,7 @@ describe ::Follow::Updater do
   fab!(:user1) { Fabricate(:user) }
   fab!(:user2) { Fabricate(:user) }
   fab!(:user3) { Fabricate(:user) }
+  fab!(:topic) { Fabricate(:topic) }
 
   it "expect users followers to include follower" do
     updater = ::Follow::Updater.new(user1, user2)
@@ -30,5 +31,36 @@ describe ::Follow::Updater do
       }.to_json
     }
     expect(user2.notifications.where(payload).where('created_at >= ?', 1.day.ago).exists?).to eq(true)
+  end
+
+  it "sent a notification for original poster and replier" do
+    updater = ::Follow::Updater.new(user1, user3)
+    updater.update(true)
+  
+    updater = ::Follow::Updater.new(user2, user3)
+    updater.update(true)
+
+    first_post = Fabricate(:post, topic: topic, user: user1)
+    second_post = Fabricate(:post, topic: topic, user: user2)
+  
+    payload = {
+      notification_type: Notification.types[:following],
+      data: {
+        display_username: user1.username,
+        following: true
+      }.to_json
+    }
+
+    expect(user3.notifications.where(payload).where('created_at >= ?', 1.day.ago).exists?).to eq(true)
+
+    payload = {
+      notification_type: Notification.types[:following],
+      data: {
+        display_username: user2.username,
+        following: true
+      }.to_json
+    }
+
+    expect(user3.notifications.where(payload).where('created_at >= ?', 1.day.ago).exists?).to eq(true)
   end
 end
