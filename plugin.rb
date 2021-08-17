9b6571f783eaa0e63cda79ece336d658ab7b21cd
Robert Barrow <merefield@gmail.com>
2021-08-17 08:12:10 +0100

    REFACTOR: improve logic

--- plugin.rb
+++ plugin.rb
@@ -126,19 +126,21 @@ after_initialize do
   
   module UserDestroyerFollowerExtension
     protected def prepare_for_destroy(user)
-      if user.following_ids.present? && SiteSetting.discourse_follow_enabled
-        user.following_ids.each do |user_id|
-          if following = User.find_by(id: user_id)
-            updater = Follow::Updater.new(user, following)
-            updater.update(false)
+      if SiteSetting.discourse_follow_enabled
+        if user.following_ids.present?
+          user.following_ids.each do |user_id|
+            if following = User.find_by(id: user_id)
+              updater = Follow::Updater.new(user, following)
+              updater.update(false)
+            end
           end
         end
-      end
-      if user.followers.present? && SiteSetting.discourse_follow_enabled
-        user.followers.each do |user_id|
-          if follower = User.find_by(id: user_id)
-            updater = Follow::Updater.new(follower, user)
-            updater.update(false)
+        if user.followers.present? && 
+          user.followers.each do |user_id|
+            if follower = User.find_by(id: user_id)
+              updater = Follow::Updater.new(follower, user)
+              updater.update(false)
+            end
           end
         end
       end
