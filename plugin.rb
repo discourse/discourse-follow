e6ea2266c024de26795c84eea5e72491f44f9252
Robert Barrow <merefield@gmail.com>
2021-08-17 08:10:36 +0100

    FIX: ensure plugin is enabled before deleting follow information

--- plugin.rb
+++ plugin.rb
@@ -126,7 +126,7 @@ after_initialize do
   
   module UserDestroyerFollowerExtension
     protected def prepare_for_destroy(user)
-      if user.following_ids.present?
+      if user.following_ids.present? && SiteSetting.discourse_follow_enabled
         user.following_ids.each do |user_id|
           if following = User.find_by(id: user_id)
             updater = Follow::Updater.new(user, following)
@@ -134,7 +134,7 @@ after_initialize do
           end
         end
       end
-      if user.followers.present?
+      if user.followers.present? && SiteSetting.discourse_follow_enabled
         user.followers.each do |user_id|
           if follower = User.find_by(id: user_id)
             updater = Follow::Updater.new(follower, user)
