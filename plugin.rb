068c62a88b1ee3d0aec05f757c7c0d0a7d8bda5f
Robert Barrow <merefield@gmail.com>
2021-08-17 07:54:04 +0100

    FIX: avoid errors when deleting users when user does not have any followers or people following them

--- plugin.rb
+++ plugin.rb
@@ -126,16 +126,20 @@ after_initialize do
   
   module UserDestroyerFollowerExtension
     protected def prepare_for_destroy(user)
-      user.following_ids.each do |user_id|
-        if following = User.find_by(id: user_id)
-          updater = Follow::Updater.new(user, following)
-          updater.update(false)
+      if user.following_ids.present?
+        user.following_ids.each do |user_id|
+          if following = User.find_by(id: user_id)
+            updater = Follow::Updater.new(user, following)
+            updater.update(false)
+          end
         end
       end
-      user.followers.each do |user_id|
-        if follower = User.find_by(id: user_id)
-          updater = Follow::Updater.new(follower, user)
-          updater.update(false)
+      if user.followers.present?
+        user.followers.each do |user_id|
+          if follower = User.find_by(id: user_id)
+            updater = Follow::Updater.new(follower, user)
+            updater.update(false)
+          end
         end
       end
       super(user)
