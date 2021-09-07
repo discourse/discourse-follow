eb74beaaee8dc584ad15dd6ba13c09108eba5f0f
Robert Barrow <merefield@gmail.com>
2021-09-07 20:15:50 +0100

    add semicolon to migration statements

--- db/migrate/20210907010101_following_data_migration.rb
+++ db/migrate/20210907010101_following_data_migration.rb
@@ -3,25 +3,24 @@ class FollowingDataMigration < ActiveRecord::Migration[5.2]
     execute <<~SQL
       UPDATE user_custom_fields
       SET value = REPLACE(value,',0',',3')
-      WHERE name = 'following'
+      WHERE name = 'following';
     SQL
     execute <<~SQL
       UPDATE user_custom_fields 
       SET value = REPLACE(value,',1',',4')
-      WHERE name = 'following'
+      WHERE name = 'following';
     SQL
   end
   def down
     execute <<~SQL
       UPDATE user_custom_fields
       SET value = REPLACE(value,',3',',0')
-      WHERE name = 'following'
+      WHERE name = 'following';
     SQL
-
     execute <<~SQL
       UPDATE user_custom_fields
       SET value = REPLACE(value,',4',',1')
-      WHERE name = 'following'
+      WHERE name = 'following';
     SQL
   end
 end
