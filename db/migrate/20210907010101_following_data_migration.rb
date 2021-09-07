ba041e0dd4a4d46d41502b9dd4aa9d237063bd58
Robert Barrow <merefield@gmail.com>
2021-09-07 20:02:28 +0100

    improve migration queries

--- db/migrate/20210907010101_following_data_migration.rb
+++ db/migrate/20210907010101_following_data_migration.rb
@@ -1,10 +1,27 @@
 class FollowingDataMigration < ActiveRecord::Migration[5.2]
   def up
-    execute "UPDATE user_custom_fields SET value = REPLACE(value,',0',',3') WHERE name = 'following'"
-    execute "UPDATE user_custom_fields SET value = REPLACE(value,',1',',4') WHERE name = 'following'"
+    execute <<~SQL
+      UPDATE user_custom_fields
+      SET value = REPLACE(value,',0',',3')
+      WHERE name = 'following'
+    SQL
+    execute <<~SQL
+      UPDATE user_custom_fields 
+      SET value = REPLACE(value,',1',',4')
+      WHERE name = 'following'
+    SQL
   end
   def down
-    execute "UPDATE user_custom_fields SET value = REPLACE(value,',3',',0') WHERE name = 'following'"
-    execute "UPDATE user_custom_fields SET value = REPLACE(value,',4',',1') WHERE name = 'following'"
+    execute <<~SQL
+      UPDATE user_custom_fields
+      SET value = REPLACE(value,',3',',0')
+      WHERE name = 'following'
+    SQL
+
+    execute <<~SQL
+      UPDATE user_custom_fields
+      SET value = REPLACE(value,',4',',1')
+      WHERE name = 'following'
+    SQL
   end
 end
