b86aa2e8d7e623a97f6fbba435b7246c835b1f89
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2023-02-21 10:53:44 +0800

    DEV: Remove use of deprecated site setting
    The site setting has been removed in core.

--- spec/system/follow_user_pages_spec.rb
+++ spec/system/follow_user_pages_spec.rb
@@ -16,26 +16,20 @@ RSpec.describe "Follow user pages", type: :system, js: true do
     Follow::Updater.new(user3, user1).watch_follow
   end
 
-  describe "when user has redesigned user page navigation enabled" do
-    before do
-      everyone_group.add(user1)
-      SiteSetting.enable_new_user_profile_nav_groups = everyone_group.name
-      sign_in(user1)
-    end
+  before { sign_in(user1) }
 
-    it "should allow user to navigate to the follow user profile pages" do
-      follow_page = PageObjects::Pages::Follow.new(user1)
-      follow_page.visit
+  it "should allow user to navigate to the follow user profile pages" do
+    follow_page = PageObjects::Pages::Follow.new(user1)
+    follow_page.visit
 
-      expect(follow_page).to have_following_topic(user2_post.topic)
+    expect(follow_page).to have_following_topic(user2_post.topic)
 
-      follow_page.click_on_followers
+    follow_page.click_on_followers
 
-      expect(follow_page).to have_follower(user3)
+    expect(follow_page).to have_follower(user3)
 
-      follow_page.click_on_following
+    follow_page.click_on_following
 
-      expect(follow_page).to have_following(user2)
-    end
+    expect(follow_page).to have_following(user2)
   end
 end
