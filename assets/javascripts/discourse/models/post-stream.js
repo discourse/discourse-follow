6c0f94f8423f4d99d9cbd0f5bcf72810bf7c1a85
Sérgio Saquetim <1108771+megothss@users.noreply.github.com>
2025-11-05 18:13:56 -0300

    DEV: Refactor `PostStream` with tracked properties and modern tools (#161)
    Replace legacy Ember constructs in `PostStream` with tracked properties,
    `@trackedArray`, and helper methods like `addUniqueValuesToArray`. Simplify
    initializations, getters, and promise handling with async/await. This
    refactor improves code clarity and aligns it with current best practices in
    modern Ember.

--- assets/javascripts/discourse/models/post-stream.js
+++ assets/javascripts/discourse/models/post-stream.js
@@ -1,9 +1,9 @@
+import { tracked } from "@glimmer/tracking";
 import EmberObject from "@ember/object";
 import { reads } from "@ember/object/computed";
-import { on } from "@ember-decorators/object";
-import { Promise } from "rsvp";
 import { ajax } from "discourse/lib/ajax";
-import discourseComputed from "discourse/lib/decorators";
+import { addUniqueValuesToArray } from "discourse/lib/array-tools";
+import { trackedArray } from "discourse/lib/tracked-tools";
 import Category from "discourse/models/category";
 import RestModel from "discourse/models/rest";
 
@@ -12,62 +12,62 @@ import RestModel from "discourse/models/rest";
 // component (in core as well) which expects a `UserStream` instance.
 
 export default class PostStream extends RestModel {
-  loading = false;
-  itemsLoaded = 0;
-  canLoadMore = true;
+  @tracked loading = false;
+  @tracked itemsLoaded = 0;
+  @tracked canLoadMore = true;
+
+  @trackedArray content = [];
 
   @reads("content.lastObject.created_at") lastPostCreatedAt;
 
-  @on("init")
-  _initialize() {
-    this.set("content", []);
+  get noContent() {
+    return !this.loading && this.content.length === 0;
   }
 
-  @discourseComputed("loading", "content.length")
-  noContent(loading, length) {
-    return !loading && length === 0;
-  }
-
-  findItems() {
+  async findItems() {
     if (!this.canLoadMore || this.loading) {
-      return Promise.resolve();
+      return;
     }
 
-    this.set("loading", true);
+    this.loading = true;
     const data = {};
+
     if (this.lastPostCreatedAt) {
       data.created_before = this.lastPostCreatedAt;
     }
-    return ajax(`/follow/posts/${this.user.username}`, { data })
-      .then((content) => {
-        const streamItems = content.posts.map((post) => {
-          return EmberObject.create({
-            title: post.topic.title,
-            postUrl: post.url,
-            created_at: post.created_at,
-            category: Category.findById(post.category_id),
-            topic_id: post.topic.id,
-            post_id: post.id,
-            post_number: post.post_number,
 
-            username: post.user.username,
-            name: post.user.name,
-            avatar_template: post.user.avatar_template,
-            user_id: post.user.id,
-
-            excerpt: post.excerpt,
-            truncated: post.truncated,
-          });
-        });
-        return { posts: streamItems, hasMore: content.extras.has_more };
-      })
-      .then(({ posts: streamItems, hasMore }) => {
-        this.content.addObjects(streamItems);
-        this.set("itemsLoaded", this.content.length);
-        this.set("canLoadMore", hasMore);
-      })
-      .finally(() => {
-        this.set("loading", false);
+    try {
+      const content = await ajax(`/follow/posts/${this.user.username}`, {
+        data,
       });
+
+      const streamItems = content.posts.map((post) => {
+        return EmberObject.create({
+          title: post.topic.title,
+          postUrl: post.url,
+          created_at: post.created_at,
+          category: Category.findById(post.category_id),
+          topic_id: post.topic.id,
+          post_id: post.id,
+          post_number: post.post_number,
+
+          username: post.user.username,
+          name: post.user.name,
+          avatar_template: post.user.avatar_template,
+          user_id: post.user.id,
+
+          excerpt: post.excerpt,
+          truncated: post.truncated,
+        });
+      });
+
+      const { hasMore } = content.extras;
+      addUniqueValuesToArray(this.content, streamItems);
+
+      this.itemsLoaded = this.content.length;
+      this.canLoadMore = hasMore;
+    } finally {
+      this.loading = false;
+    }
   }
 }
