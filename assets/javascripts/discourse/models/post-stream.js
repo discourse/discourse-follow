c7f5f1afcad75e3ecc214c371cfd76b5c476c13e
Sérgio Saquetim <1108771+megothss@users.noreply.github.com>
2025-12-02 16:09:04 -0300

    DEV: Replace deprecated array methods in `post-stream.js` (#162)
    Refactor `post-stream.js` to eliminate deprecated array methods, enhancing code stability and maintainability. The changes include replacing `set` with tracked properties and utilizing `addUniqueValuesToArray` for managing the content array.

--- assets/javascripts/discourse/models/post-stream.js
+++ assets/javascripts/discourse/models/post-stream.js
@@ -1,6 +1,6 @@
 import { tracked } from "@glimmer/tracking";
 import EmberObject from "@ember/object";
-import { reads } from "@ember/object/computed";
+import { dependentKeyCompat } from "@ember/object/compat";
 import { ajax } from "discourse/lib/ajax";
 import { addUniqueValuesToArray } from "discourse/lib/array-tools";
 import { trackedArray } from "discourse/lib/tracked-tools";
@@ -12,14 +12,17 @@ import RestModel from "discourse/models/rest";
 // component (in core as well) which expects a `UserStream` instance.
 
 export default class PostStream extends RestModel {
-  @tracked loading = false;
-  @tracked itemsLoaded = 0;
   @tracked canLoadMore = true;
-
+  @tracked itemsLoaded = 0;
+  @tracked loading = false;
   @trackedArray content = [];
 
-  @reads("content.lastObject.created_at") lastPostCreatedAt;
+  @dependentKeyCompat
+  get lastPostCreatedAt() {
+    return this.content.at(-1)?.created_at;
+  }
 
+  @dependentKeyCompat
   get noContent() {
     return !this.loading && this.content.length === 0;
   }
@@ -31,16 +34,13 @@ export default class PostStream extends RestModel {
 
     this.loading = true;
     const data = {};
-
     if (this.lastPostCreatedAt) {
       data.created_before = this.lastPostCreatedAt;
     }
-
     try {
       const content = await ajax(`/follow/posts/${this.user.username}`, {
         data,
       });
-
       const streamItems = content.posts.map((post) => {
         return EmberObject.create({
           title: post.topic.title,
@@ -60,12 +60,9 @@ export default class PostStream extends RestModel {
           truncated: post.truncated,
         });
       });
-
-      const { hasMore } = content.extras;
       addUniqueValuesToArray(this.content, streamItems);
-
       this.itemsLoaded = this.content.length;
-      this.canLoadMore = hasMore;
+      this.canLoadMore = content.extras.has_more;
     } finally {
       this.loading = false;
     }
